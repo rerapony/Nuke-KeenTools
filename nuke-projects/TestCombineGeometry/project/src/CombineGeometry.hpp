ce11352d1b5c5d949cd139ffef0da1b22e87c3b0
valeria <urgbal@gmail.com>
2020-04-18 05:25:05 +0300

    split into header and source files

---
+++ nuke-projects/TestCombineGeometry/project/src/CombineGeometry.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "DDImage/GeoOp.h"
+#include "DDImage/Scene.h"
+#include "DDImage/Knobs.h"
+#include "DDImage/ViewFrustum.h"
+
+#include <cassert>
+
+using namespace DD::Image;
+
+static const char* const CLASS = "CombineGeometry";
+static const char* const HELP = "Combine geometries of two objects";
+
+
+class CombineGeometry : public GeoOp
+{
+	float _param;
+
+protected:
+	void _validate(bool for_real) override;
+
+public:
+	static const Description description;
+	const char* Class() const override;
+	const char* node_help() const override;
+
+	CombineGeometry(Node* node);
+
+	int minimum_inputs() const override;
+	int maximum_inputs() const override;
+
+	void knobs(Knob_Callback) override;
+
+	void get_geometry_hash() override;
+
+	void geometry_engine(Scene& scene, GeometryList& out) override;
+};
