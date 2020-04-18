4280550a4fca93b055fe7701364b6139074131ae
valeria <urgbal@gmail.com>
2020-04-18 05:57:26 +0300

    .gitignore

--- nuke-deps/include/Build/Compiler/fnVC8.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2009 The Foundry Visionmongers Ltd.  All Rights Reserved.
-
-#ifndef __Build_fnVC8_h__
-#define __Build_fnVC8_h__
-
-#include "fnVC.h"
-
-#endif
--- nuke-projects/TestBlur/build/cmake_install.cmake
+++ nuke-projects/TestBlur/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Work/nuke-projects/TestBlur/nuke-practice
+# Install script for directory: D:/Work/nuke-practice/nuke-projects/TestBlur/project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Work/nuke-projects/TestBlur/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Work/nuke-practice/nuke-projects/TestBlur/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
